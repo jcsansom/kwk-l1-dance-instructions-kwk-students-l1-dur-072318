cde2a70d52735497f0a14b9ab52dc1c875091b4c
jcsansom <jayceesansom@gmail.com>
2018-07-23 20:40:57 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,63 +1,66 @@
-
 # Code your methods below
 
-def Gangnam_Style
-  Lower_Body
-  Skip-Step
-  Upper_Body
-  Bust_A_Move
+
+
+def lower_body
+  starting_stance
+  base_footwork
 end
 
-def Lower_Body
-  Assume_the_proper_stance
-  Base_Footwork
+def starting_stance
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
 end
 
-def Assume_the_proper_stance
-  puts "Plant legs far apart, bend knees slightly and keep posture loose"
-end
-
-def Base_Footwork
+def base_footwork
   puts "Lift right foot"
   puts "Return right food to the ground"
   puts "Finishing with a small Skip-Step backward"
 end
 
-def Skip-Step
-  Skip-Step_Left
-  Skip-Step_Right
+def skip_step
+  skip_step_left
+  skip_step_right
 end
 
-def Skip-Step_Left
+def skip_step_left
   puts "Lower left foot to the ground"
   puts "Bounce left foot bak up slightly, kicking it a few inches back"
   puts "Left, Right, Left and Left"
 end
 
-def Skip-Step_Right
+def skip_step_right
   puts "Lower right foot to the ground"
-  puts "Boune right foot back up slightly, kicking it a few inches back"
+  puts "Bounce right foot back up slightly, kicking it a few inches back"
   puts "Right, Left, Right, and Right"
 end
 
-def Upper_Body
-  Bob_the_reins
-  Lasso 
+def upper_body
+  bob_the_reins
+  lasso 
 end
 
-def Bob_the_reins
+def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
 end
 
-def Lasso
+def lasso
   puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
   puts "Lift right arm up and point right elbow diagonally to the right."
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 end
 
-def Bust_A_Move
+def bust_a_move
   puts "Be cool"
   puts "Be snazzy!"
+end
+
+def gangnam_style
+  starting_stance
+  bust_a_move
+  base_footwork
+  bob_the_reins + skip_step * 8
+  lasso + skip_step * 8
+  
 end
